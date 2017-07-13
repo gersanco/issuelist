724597c0d24ce28e4861bb873fc7e6e05f9cf81d
German San Emeterio <german@incotel.es>
2017-07-13 17:55:10 +0200

    Paginacion de las issues traidas

--- src/app/components/issue-list/issue-list.component.ts
+++ src/app/components/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IssuePage } from "../../models/issue-page";
 import { Repository } from "../../models/repository";
 import { IssueService } from '../../services/issue.service';
 import { SharedService  } from '../../services/shared.service';
@@ -9,12 +10,18 @@ import { SharedService  } from '../../services/shared.service';
   providers: [IssueService]
 })
 export class IssueListComponent implements OnInit {
+  public pages:number;
+  public issuesPag:any[];
+  private issuePerPage:Number;
   public repository: Repository;
   public issues:any[];
   constructor(
     private _issueService: IssueService,
     private _sharedService: SharedService
   ) {
+    this.pages = 1;
+    this.issuePerPage = 8;
+    this.issuesPag = [];
     _sharedService.changeEmitted$.subscribe(
       resp => {
         this.repository = resp;
@@ -30,11 +37,40 @@ export class IssueListComponent implements OnInit {
     this._issueService.getIssues(url).subscribe(
       resp => {
         this.issues = resp;
+        if(this.issues.length > 0) {
+          let pag = 1;
+          let issePg = new IssuePage(pag, []);
+          for(let issue of this.issues) {
+            issePg.issues.push(issue);
+            if(issePg.issues.length == this.issuePerPage) {
+              this.issuesPag.push(issePg);
+              pag++;
+              issePg = new IssuePage(pag, []);
+            }
+          }
+          console.log(this.issuesPag)
+          this.issues = this.issuesPag[0].issues;
+        }
         console.log(this.issues)
       }, err => {
         console.log(err);
       }
     )
+  }
+
+  last() {
+    this.issues = this.issuesPag[this.issuesPag.length-1].issues;
+    this.pages = this.issuesPag.length;
+  }
+
+  first() {
+    this.issues = this.issuesPag[0].issues;
+    this.pages = 1;
+  }
+
+  setPage(pag) {
+    this.issues = pag.issues;
+    this.pages = pag.pag;
   }
 
 }
---
+++ src/app/models/issue-page.ts
@@ -0,0 +1,6 @@
+export class IssuePage {
+  constructor(
+    public pag:Number,
+    public issues: any[]
+  ) {}
+}
